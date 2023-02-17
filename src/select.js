6d4522dea796d97170af9c783f64bb543baef9fb
ezdub <ezdub@users.noreply.github.com>
2023-02-16 19:17:01 -0600

    patched to add theme to body class

--- src/select.js
+++ src/select.js
@@ -2,6 +2,7 @@ function themeSelect() {
   (function (theme = localStorage.getItem("theme")) {
     if (localStorage.getItem("theme")) {
       document.documentElement.setAttribute("data-theme", theme);
+      document.body.classList.add(theme);
       var optionToggler = document.querySelector("select[data-choose-theme] [value='" + theme.toString() + "']");
       if (optionToggler) {
         [...document.querySelectorAll("select[data-choose-theme] [value='" + theme.toString() + "']")].forEach((el) => {
@@ -14,6 +15,8 @@ function themeSelect() {
     [...document.querySelectorAll("select[data-choose-theme]")].forEach((el) => {
       el.addEventListener('change', function () {
         document.documentElement.setAttribute("data-theme", this.value);
+        document.body.classList.remove("dark","light","auto");
+        document.body.classList.add(this.value);
         localStorage.setItem("theme", document.documentElement.getAttribute('data-theme'));
         [...document.querySelectorAll("select[data-choose-theme] [value='" + localStorage.getItem("theme") + "']")].forEach((el) => {
           el.selected = true;
